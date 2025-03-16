49b3c3d4095cb6d5db4cf5fabca41fde1f9e7368
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-16 16:46:52 +0530

    initial backend commit

---
+++ harmoniq_backend/src/main/java/dev/anirban/harmoniq_backend/HarmoniqBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.anirban.harmoniq_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmoniqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmoniqBackendApplication.class, args);
+	}
+
+}
---
+++ harmoniq_backend/src/test/java/dev/anirban/harmoniq_backend/HarmoniqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anirban.harmoniq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmoniqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
