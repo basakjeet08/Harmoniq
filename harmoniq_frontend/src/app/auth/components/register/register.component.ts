a5180b1321bda72b10a0a33dee3cb9542b3c919d
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-16 18:31:18 +0530

    feat : Implemented the Register UI Layer.

--- harmoniq_frontend/src/app/auth/components/register/register.component.ts
+++ harmoniq_frontend/src/app/auth/components/register/register.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  // These are the details inputted by the user
+  userInput = { email: '', password: '', confirmPassword: '' };
+
+  // Error and loading states
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
+
+  // Injecting the necessary dependencies
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // This function is invoked when the user clicks the register button
+  onRegisterClick() {
+    console.log(this.userInput);
+  }
+
+  // This function is invoked when the user clicks on the go to login button
+  onGoToLoginClick() {
+    this.router.navigate(['../', 'login'], { relativeTo: this.route });
+  }
+
+  // This function is invoked when the user clicks on the cancel error button
+  onErrorCancelClick() {
+    this.errorMessage = null;
+  }
+}
