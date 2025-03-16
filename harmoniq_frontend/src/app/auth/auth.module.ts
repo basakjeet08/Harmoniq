41b14d6bfaca00b18ee389d6c953e4e995cd816d
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-16 18:05:48 +0530

    feat : Configured the auth module routes and implemented the login UI.

--- harmoniq_frontend/src/app/auth/auth.module.ts
+++ harmoniq_frontend/src/app/auth/auth.module.ts
@@ -2,12 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 // These are the routes for the Auth Components
-const authRoutes: Routes = [{ path: '', component: AuthComponent }];
+const authRoutes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [AuthComponent],
-  imports: [CommonModule, RouterModule.forChild(authRoutes)],
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(authRoutes),
+    FormsModule,
+    SharedModule,
+  ],
 })
 export class AuthModule {}
---
+++ harmoniq_frontend/src/app/auth/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  // These are the details inputted by the user
+  userInput = { email: '', password: '' };
+
+  // Loading and Error States
+  isLoading: boolean = false;
+  errorMessage: string | null = null;
+
+  // Injecting the necessary dependencies
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // This function is invoked when the user clicks the login button
+  onLoginClick() {
+    console.log(this.userInput);
+  }
+
+  // This function is invoked when the user clicks on login as Guest Button
+  onLoginAsGuestClick() {
+    console.log('Login As Guest Clicked');
+  }
+
+  // This function is invoked when the user clicks on the go to Register button
+  onGoToRegisterClick() {
+    this.router.navigate(['../', 'register'], { relativeTo: this.route });
+  }
+
+  // This function is invoked when the user clicks on the cancel Error Button
+  onErrorCancelClick() {
+    this.errorMessage = null;
+  }
+}
