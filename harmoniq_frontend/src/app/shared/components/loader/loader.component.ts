98fd7f25babcd3f4c717112c817923703aae3656
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-22 21:19:58 +0530

    update : Updated the Loading UI to a slide up and down toast.

--- harmoniq_frontend/src/app/shared/components/loader/loader.component.ts
+++ harmoniq_frontend/src/app/shared/components/loader/loader.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { slideDownAndUpAnimation } from '../animations/slide-down-animation';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css'],
+  animations: [slideDownAndUpAnimation],
 })
 export class LoaderComponent {
   // This variable stores if the loader should be visible or not
