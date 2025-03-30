1fdd02d649a1233490793bc3e863e62670763487
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-30 13:58:46 +0530

    feat : Implemented the Edit Profile and Delete Account Feature.

--- harmoniq_frontend/src/app/profile/profile.component.ts
+++ harmoniq_frontend/src/app/profile/profile.component.ts
@@ -3,8 +3,9 @@ import { UserService } from '../shared/services/user.service';
 import { LoaderService } from '../shared/components/loader/loader.service';
 import { ToastService } from '../shared/components/toast/toast.service';
 import { UserDto } from '../shared/Models/user/UserDto';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProfileService } from '../shared/services/profile.service';
+import { Roles } from '../shared/Models/user/Roles';
 
 @Component({
   selector: 'app-profile',
@@ -15,6 +16,8 @@ export class ProfileComponent implements OnInit {
   // This is the data for the component
   userData!: UserDto;
   isOwner: boolean = false;
+  isGuestMode: boolean = false;
+  isEditMode: boolean = false;
 
   // Injecting the necessary dependencies
   constructor(
@@ -22,13 +25,13 @@ export class ProfileComponent implements OnInit {
     private profileService: ProfileService,
     private loaderService: LoaderService,
     private toastService: ToastService,
+    private router: Router,
     private route: ActivatedRoute
   ) {}
 
   // Fetching the User Details on comonent init
   ngOnInit(): void {
-    const userId = this.route.snapshot.params['id'];
-    this.fetchUserData(userId);
+    this.route.params.subscribe((params) => this.fetchUserData(params['id']));
   }
 
   // This function fetches the user data from the database
@@ -43,6 +46,8 @@ export class ProfileComponent implements OnInit {
         this.loaderService.endLoading();
         this.userData = userData;
 
+        this.isGuestMode = userData.role === Roles.GUEST;
+
         // If the current user at view viewing his own profile
         if (this.userData.id === this.profileService.getUser()?.id) {
           this.isOwner = true;
@@ -52,6 +57,69 @@ export class ProfileComponent implements OnInit {
           type: 'success',
           message: 'User data fetched successfully !!',
         });
+      },
+
+      // Error State
+      error: (error: Error) => {
+        this.loaderService.endLoading();
+        this.toastService.showToast({ type: 'error', message: error.message });
+      },
+    });
+  }
+
+  // This function is invoked when the user clicks on the edit button
+  onEditToggle() {
+    this.isEditMode = !this.isEditMode;
+
+    if (this.isEditMode !== true) {
+      this.fetchUserData(this.userData.id);
+    }
+  }
+
+  // This function is invoked when the user clicks on the delete button
+  onDeleteClick() {
+    // Starting the loading state
+    this.loaderService.startLoading();
+
+    this.userService.deleteUser().subscribe({
+      // Success State
+      next: () => {
+        this.loaderService.endLoading();
+        this.profileService.logout();
+
+        this.toastService.showToast({
+          type: 'success',
+          message: 'User deleted successfully !!',
+        });
+
+        this.router.navigate(['/auth']);
+      },
+
+      // Error State
+      error: (error: Error) => {
+        this.loaderService.endLoading();
+        this.toastService.showToast({ type: 'error', message: error.message });
+      },
+    });
+  }
+
+  // This function is invoked when the user clicks on the submit edition
+  onEditComplete() {
+    // Starting the loading state
+    this.loaderService.startLoading();
+
+    this.userService.updateUser(this.userData).subscribe({
+      // Success State
+      next: () => {
+        this.loaderService.endLoading();
+
+        this.toastService.showToast({
+          type: 'success',
+          message: 'User Edited successfully !!',
+        });
+
+        this.isEditMode = false;
+        this.fetchUserData(this.userData.id);
       },
 
       // Error State
