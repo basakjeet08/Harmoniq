1bdf7bc20f03744295b4d8557abad4f01a6cee6d
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-30 22:34:54 +0530

    feat : Implemented clicking on the avatars at threads feature shows the user account.

--- harmoniq_frontend/src/app/threads/components/comment-card/comment-card.component.ts
+++ harmoniq_frontend/src/app/threads/components/comment-card/comment-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comment-card',
@@ -7,8 +8,17 @@ import { Component, Input } from '@angular/core';
 })
 export class CommentCardComponent {
   // Input Fields which will be passed by the parent component
+  @Input('creatorId') creatorId: string = '';
   @Input('creatorName') creatorName: string = '';
   @Input('creatorRole') creatorRole: string = '';
   @Input('creatorAvatar') creatorAvatar: string = '';
   @Input('comment') comment: string = '';
+
+  // Injecting the necessary dependencies
+  constructor(private router: Router) {}
+
+  // This function is invoked when the user clicks on the user avatar
+  onAvatarClick() {
+    this.router.navigate(['/dashboard/profile', this.creatorId]);
+  }
 }
