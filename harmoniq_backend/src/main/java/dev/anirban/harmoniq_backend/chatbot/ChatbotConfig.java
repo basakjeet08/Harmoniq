8c39dca12a04e87f99e01a71b3b493800ec2505d
Anirban Basak <jeetbasak2002@gmail.com>
2025-04-03 00:56:26 +0530

    feat : Added environment variables for normal, dev , docker and prod.

--- harmoniq_backend/src/main/java/dev/anirban/harmoniq_backend/chatbot/ChatbotConfig.java
+++ harmoniq_backend/src/main/java/dev/anirban/harmoniq_backend/chatbot/ChatbotConfig.java
@@ -4,9 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.ai.ollama.api.OllamaModel;
-import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,21 +13,6 @@ public class ChatbotConfig {
 
     // Injecting the chat messaging service for the chatbot
     private final CustomChatMemoryImpl customChatMemoryImpl;
-
-    @Bean
-    OllamaChatModel ollamaChatModel() {
-        return OllamaChatModel
-                .builder()
-                .ollamaApi(new OllamaApi("http://localhost:11434"))
-                .defaultOptions(
-                        OllamaOptions
-                                .builder()
-                                .model(OllamaModel.MISTRAL)
-                                .temperature(0.5)
-                                .build()
-                )
-                .build();
-    }
 
     @Bean
     ChatClient chatClient(OllamaChatModel ollamaChatModel) {
