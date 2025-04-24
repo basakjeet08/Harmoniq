6d9b80ba088f58a717d559e9756d32af8d248a53
Anirban Basak <jeetbasak2002@gmail.com>
2025-04-24 13:29:21 +0530

    update : remove pagination for fetching all tags endpoint.

--- harmoniq_backend/src/main/java/dev/anirban/harmoniq_backend/controllers/TagController.java
+++ harmoniq_backend/src/main/java/dev/anirban/harmoniq_backend/controllers/TagController.java
@@ -6,13 +6,12 @@ import dev.anirban.harmoniq_backend.dto.tag.TagDto;
 import dev.anirban.harmoniq_backend.entity.threads.Tag;
 import dev.anirban.harmoniq_backend.service.threads.TagService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,14 +21,14 @@ public class TagController {
     private final TagService service;
 
     @GetMapping(UrlConstants.FETCH_ALL_TAGS)
-    public ResponseWrapper<Page<TagDto>> handleFetchAllTagsRequest(
-            @AuthenticationPrincipal UserDetails userDetails,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
+    public ResponseWrapper<List<TagDto>> handleFetchAllTagsRequest(
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
-        Page<TagDto> tagDtoPage = service
-                .fetchAllTags(PageRequest.of(page, size))
-                .map(Tag::toTagDto);
+        List<TagDto> tagDtoPage = service
+                .fetchAllTags()
+                .stream()
+                .map(Tag::toTagDto)
+                .toList();
 
         return new ResponseWrapper<>("All Tags fetched successfully !!", tagDtoPage);
     }
